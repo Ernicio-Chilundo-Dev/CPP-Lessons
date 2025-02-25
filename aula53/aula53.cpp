d53877f19c9cd7b4117517c4a5feee0b3415be60
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-25 20:48:48 +0200

    aula 53 feita com sucesso aprendi a trabalhar com iterator

--- aula53/aula53.cpp
+++ aula53/aula53.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 int main()
 {
-    vector<string> produntos = {"mouse", "teclado", "monitor", "gabinente", "cx.som"};
-    cout << produntos[0] << endl;
+    vector<string> produtos = {"mouse", "monitor", "teclado", "gabinete", "cx.som"};
+    vector<string>::iterator it;
+    for (it = produtos.begin(); it != produtos.end(); it++)
+    {
+        cout << *it << endl;
+    }
+    // advance
+    // next
+    // prev
+    // advance(it,2);
+    // cout << *next(it,2) << endl;
+    // cout << *prev(it,1) << endl;
     return 0;
 }
