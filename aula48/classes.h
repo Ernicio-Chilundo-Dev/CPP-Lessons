6b9f7ced495c06dc47e70291f57cf0f1d8bfafd7
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-25 09:46:02 +0200

    aula 48 feita com sucesso aprendi a trabalhar com classes de herenca multipla

--- aula48/classes.h
+++ aula48/classes.h
@@ -1,5 +1,26 @@
 #ifndef CLASES_H_INCLUDE
-#defne CLASSES_H_INCLUDE
+#define CLASSES_H_INCLUDE
 
+class Base1{
+    public:
+    void impBase1();
+};
+
+void Base1::impBase1(){
+    std::cout <<"Tmp class Base1"<<std::endl;
+}
+
+class Base2{
+    public:
+    void impBase2();
+};
+
+void Base2::impBase2(){
+    std::cout << "Tmp class Base2"<<std::endl;
+}
+
+class Cfb:public Base1, public Base2{
+
+};
 
 #endif
