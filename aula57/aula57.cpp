5c1908b1d66928bb3c872ffc867cf8c85fb80435
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-27 00:37:37 +0200

    aula 57 feita com sucesso aprende a trabalhar com classes de armazenamento "extern"

--- aula57/aula57.cpp
+++ aula57/aula57.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include  "variaveis.h"
 using namespace std;
-
-int main(){
-
+void impNum();
+int num{50};
+extern int valor;
+int main()
+{
+    impNum();
+    cout << valor << endl;
+    cout << tmp << endl;
     return 0;
 }
---
+++ aula57/variaveis.h
@@ -0,0 +1,5 @@
+#ifndef VARIAVEIS_H_INCLUDE
+#define VARIAVEIS_H_INCLUDE
+
+int tmp = 100;
+#endif
