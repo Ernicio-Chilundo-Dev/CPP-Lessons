113bf4426e23914ae9ea2527272ba3b0b5d59484
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-28 19:10:28 +0200

    aula 64 aprendendo a trabalhar com classes em c++ moderno

--- aula64/aula64.cpp
+++ aula64/aula64.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <memory>
 using namespace std;
+class Carro
+{
+public:
+    int velMax;
+    int potencia;
+    const char *nome;
 
-int main(){
+    Carro(const char *n, int p) : nome(n), potencia(p)
+    {
+        if (p < 100)
+        {
+            this->velMax = 120;
+        }
+        else if (p < 200)
+        {
+            this->velMax = 220;
+        }
+        else
+        {
+            this->velMax = 350;
+        }
+    }
+};
+int main()
+{
+
+    unique_ptr<Carro>c1(new Carro("Izuzu",70));
+    cout << c1->nome << " - " << c1->potencia << " - " << c1->velMax << endl;
 
     return 0;
 }
