c515c37482197c400988ed08d8b2646f01f193c9
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-01 05:49:07 +0200

    iniciado a aula 68 que vou aprender a trabalhar com o modificador de acesso protected junto dos outros modificadores private e public.

---
+++ aula68/aula68.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Veiculo{
+    private:
+    int velMax;
+    int potencia;
+    public:
+    int rodas;
+    const char* nome;
+
+    protected:
+    int portas;
+    const char* cor;
+};
+
+int main(){
+
+    return 0;
+}
