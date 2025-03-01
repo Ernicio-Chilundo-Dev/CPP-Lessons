680df50ac4bc826298da6bdbb60f263bdde08d40
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-01 08:25:25 +0200

    aula 69 feita com sucesso aprendi a trabalhar com o algorithm find e tive uma boa explicacao de como o size_t funciona

--- aula69/aula69.cpp
+++ aula69/aula69.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int vetor[] = {6, 4, 5, 2, 8, 9, 3};
-    int *p;
+    // int vetor[] = {6, 4, 5, 2, 8, 9, 3};
+    // int *p;
 
-    size_t tam = sizeof vetor / 4;
-    p = find(vetor, vetor + tam, 7);
-    if (p != vetor + tam)
-    {
-        cout << "Numeral encontrado: " << *p << endl;
+    // size_t tam = sizeof vetor / 4;
+    // p = find(vetor, vetor + tam, 7);
+    // map<char, int> mapa;
+    // mapa['a'] = 10;
+    // mapa['b'] = 33;
+    // mapa['c'] = 50;
+    // mapa['e'] = 20;
+    // mapa['f'] = 5;
+
+    // auto it = mapa.find('c');
+    // if (it != mapa.end())
+    // {
+    //     cout << "Chave encontrada: " << it->second << endl;
+    // }
+    // else
+    // {
+    //     cout << "Chave nao encontarada!";
+    // }
+
+    // NB: size_t >> e um elemento proprio para armazenar tamanho
+    string texto = "As ondas do mar me acalmam";
+    size_t enc = texto.find("oceano");
+    if(enc!=string::npos){
+        cout << "Palavra encontrada: "<< enc<<endl;
+    }else{
+        cout <<"Palavra nao encontarda!";
     }
-    else
-    {
-        cout << "Numeral Nao encontrado!";
-    }
+ 
     return 0;
 }
