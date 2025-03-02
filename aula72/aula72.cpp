975fd86dc436642b1fd96da5e9536907a18ac693
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-02 17:07:44 +0200

    aula 72 feita com sucesso aprendi que a bliblioteca iostream trabalha com o fluxo de entrada e saida de dados

--- aula72/aula72.cpp
+++ aula72/aula72.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main(){
 
-    char n,s;
+    // char n,s;
 
-    n=cin.get();
-    cin.ignore();
-    s=cin.get();
+    // n=cin.get();
+    // cin.ignore();
+    // s=cin.get();
 
-    cout << n << " - " << s<<endl;
+    // cout << n << " - " << s<<endl;
+
+    char nome[80];
+    cin.get(nome,80);
+
+    cout <<nome<<" = " << cin.gcount();
     return 0;
 }
