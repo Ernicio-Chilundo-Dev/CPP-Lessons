376828cfdae63ddf75f14c3aa3bdef1277f4327c
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-02 17:17:27 +0200

    iniciado a aula 73 que  fala da bibliioteca cstdlib

---
+++ aula73/aula73.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    double num;
+
+    char numero[80];
+
+    cin >> numero;
+
+    num = atof(numero);
+
+    cout << num<<endl;
+
+    return 0;
+}
