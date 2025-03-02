72738f1d9259240b4ac677851f8ee5bfd37fd8b7
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-02 17:51:44 +0200

    aula 73 feita com sucesso aprendi que a biblioteca cstdlib implementa as funcoes de conversao, gerecao de numeros aleatorios e de gerenciamento dinamico da memoria

--- aula73/aula73.cpp
+++ aula73/aula73.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int main(){
-    double num;
+int main()
+{
+    // double num;
 
-    char numero[80];
+    // char numero[80];
 
-    cin >> numero;
+    // cin >> numero;
 
-    num = atof(numero);
+    // // num = atof(numero);
+    // num = strtod(numero, NULL);
 
-    cout << num<<endl;
+    // cout << num << endl;
 
+    // funcoes de conversao
+    // atof atoi atol atoll(c++11);
+    // astrtod astrtof astrtol astrtoll;
+
+    // funcoes que geram numeros aleatorios
+
+    // srand(time(NULL));
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << rand() % 100 << endl;
+    // }
+
+    // funcoews de gerenciamento de dinamico da memoria
+    // calloc malloc
+    //  free realloc
+
+    int tam = 10;
+    int *vetor;
+
+    vetor = (int *)calloc(tam, sizeof(int));
+
+    vetor = (int *)malloc(sizeof(int));
+
+    srand(time(NULL));
+    for (int i = 0; i < tam; i++)
+    {
+        vetor[i] = rand() % 100;
+        cout << vetor[i] << "\n";
+    }
+    free(vetor);
     return 0;
 }
