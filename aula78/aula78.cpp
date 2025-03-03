23a9b93227e56fbaaa4e7642aedb9026f390817a
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-03 18:38:46 +0200

    aula 78 feita com sucesso aprendi a trabalahr com as funcoes swap que faz a traca dos valores de uma variavel  e a funcao maike_pair que constroi pares de de elementos e depende do tipo pair que e pares de valores

--- aula78/aula78.cpp
+++ aula78/aula78.cpp
@@ -2,12 +2,24 @@
 #include <utility>
 using namespace std;
 int main(){
-    int a,b;
-    a= 12;
-    b = 8;
+    // int a,b;
+    // a= 12;
+    // b = 8;
 
-    swap(a,b);
-        cout << "Valor de a: "<<a<<" - Valor de b: "<< b<<endl;
+    // swap(a,b);
+    //     cout << "Valor de a: "<<a<<" - Valor de b: "<< b<<endl;
+
+    pair<int,int>p1;
+    pair<int,char>p2;
+    pair<int,string>p3;
+
+    p1=make_pair(10,5);
+    p2=make_pair(1,'E');
+    p3=make_pair(2,"Ernicio");
+
+    cout<<"Par 1: " << p1.first<< " - " << p1.second<<endl;
+    cout <<"Par 2: "<< p2.first<< " = " << p2.second<< endl;
+    cout<<"Par 3: " << p3.first <<" - " << p3.second<<endl;
 
     return 0;
 }
