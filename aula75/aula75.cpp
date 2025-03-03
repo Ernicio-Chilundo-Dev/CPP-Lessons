9f6daa01aac8d9b3398fd4aaf9b655839ae842ca
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-03 10:45:13 +0200

    aula 75 feita com sucesso, usei a funcao artimetica implimentada na biblioteca cstdlib

--- aula75/aula75.cpp
+++ aula75/aula75.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
     // int n = -10;
     // cout << abs(n)<<endl;
 
-    int num,dem;
+    size_t tam;
+    vector<int> numeros = {9, 8, 5, 4, 3, 2, 1, 6, 7, 0};
+
+    tam = numeros.size();
+    cout << tam << endl;
+
+    int num, dem;
     div_t res;
     num = 10;
     dem = 3;
 
-    res = div(num,dem);
-    cout <<num<<" devidido por "<< dem <<" = "<< res.quot<< " e o resto da divisao e "<< res.rem<<endl;
+    res = div(num, dem);
+    cout << num << " devidido por " << dem << " = " << res.quot << " e o resto da divisao e " << res.rem << endl;
     return 0;
 }
