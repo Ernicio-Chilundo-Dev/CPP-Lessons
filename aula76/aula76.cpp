5ec51bb2fd92e9b103bc6b464b96c85b0c9801a3
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-03 14:54:08 +0200

    aula terminada com sucesso aprendi que a biblioteca typeinfo c++ fornece informacoes do tipo em tempo de execucao

--- aula76/aula76.cpp
+++ aula76/aula76.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <typeinfo>
 using namespace std;
+class Carro{};
 
 int main(){
-auto num{10};
+auto num{10.9};
+if(typeid(int).before(typeid(double))){
+    cout << "Int vem antes de double!"<<endl;
+}else{
+    cout << "double vem antes do Int"<<endl;
+}
 cout << "Tipo do num: "<< typeid(num).name()<<endl;
     return 0;
 }
