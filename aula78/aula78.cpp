fe63f0e2bbc3311d27354a1d5eed3b6d8c4cad01
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-03 18:20:25 +0200

    iniciado a aula 78 que fala sobre a biblioteca utlity

---
+++ aula78/aula78.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+int main(){
+    int a,b;
+    a= 12;
+    b = 8;
+
+    swap(a,b);
+        cout << "Valor de a: "<<a<<" - Valor de b: "<< b<<endl;
+
+    return 0;
+}
