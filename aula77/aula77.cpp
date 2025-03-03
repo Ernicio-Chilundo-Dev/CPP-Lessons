9bb463dc4a07feddc5f5ab4600772537de4736a6
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-03 16:34:36 +0200

    aula 77 feita com sucesso aprendi a trabalhar com a biblioteca functional onde usei os elementos bind que cria uma ligacao entre uma funcao e uma variavel, cref que usa a mesma referencia para dois elementos e mem_fn que e uma funcao adptadora usada para criar obejetos de funcao que podem invocar metodos membros de um obejeto

--- aula77/aula77.cpp
+++ aula77/aula77.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <functional>
 using namespace std;
+class Cfb
+{
+public:
+    int num;
+    Cfb(int n) : num(n) {}
+    int dobro()
+    {
+        return num * 2;
+    }
+};
 
-int soma(int n1,int n2){
+int soma(int n1, int n2)
+{
     return n1 + n2;
 }
 
-int main(){
-    int n1 =10;
-    const int n2 =5;
+int main()
+{
+    // int n1 =10;
+    // // const int n2 =5;
 
-    auto s1 = bind(soma,n1,n2);
-    cout << s1()<<endl;
+    // // auto s1 = bind(soma,n1,n2);
+    // // cout << s1()<<endl;
+    // auto n2 = cref(n1);
+    // n1+=9;
+    // cout << n2<<endl;
+
+    Cfb n1{10};
+    Cfb n2{5};
+    auto dobro2 = mem_fn(&Cfb::dobro);
+    cout << dobro2(n2);
 
     return 0;
 }
