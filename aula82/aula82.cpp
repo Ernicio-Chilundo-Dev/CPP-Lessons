75295fd8b56645b4d5c61929ab426fe0c104451e
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-04 18:27:20 +0200

    aula 82 feita com sucesso trabalei com a biblioteca string que tem varias funcoes de manipulacao entre elas a a funcao strcant que junta uma string em outro elemento

--- aula82/aula82.cpp
+++ aula82/aula82.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 
 int main(){
-    char txt1[50]="Curso de programacao!";
-    char txt2[50];
-    strcpy(txt2,txt1);
-    cout << txt2<<endl;
+    char txt1[]="Curso de programacao - c++";
+    char txt2[]="Curso de programacao - c++";
+    int res;
+    res = memcmp(txt1,txt2,sizeof(txt1));
+    // memcpy(pessoa1.nome,meunome,strlen(meunome)+1);
+    // memcpy(pessoa2.nome,pessoa1.nome,sizeof(pessoa1.nome)+1);
+    // cout << pessoa2.nome<<endl;
+
+    if(res ==0){
+        cout << "Iguais";
+    }else{
+        cout<< "Diferentes";
+    }
     return 0;
 }
