3553dba723f5e9e61b7d60648255339ff9e15d3a
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-04 22:28:50 +0200

    aula feita

--- aula83/aula83.cpp
+++ aula83/aula83.cpp
@@ -2,16 +2,27 @@
 #include <cstring>
 using namespace std;
 
-int main(){
-    char * txt1;
-    char txt2[]="Ernicio";
-    char pesquisa = 'B';
-
-    txt1 = (char*)memchr(txt2,pesquisa,strlen(txt2));
-    if(txt1!=NULL){
-        cout <<"Letra: "<<pesquisa <<" encontrada na posicao: "<<txt1 - txt2+1<<endl;
-    }else{
-        cout << "Letra: "<<pesquisa<< " nso encontrada";
+int main()
+{
+    char txt1[] = "ERNICIO, jermias - chilundo";
+    char * c;
+    c= strtok(txt1,",-");
+    while (c!= NULL)
+    {
+        cout << c;
+        c= strtok(NULL,",-");
     }
+    
+    // char pesquisa = 'I';
+    // txt2 = strchr(txt1, pesquisa);
+
+    // cout << "Posicao: " << txt1 - txt2+1;
+
+    // txt1 = (char*)memchr(txt2,pesquisa,strlen(txt2));
+    // if(txt1!=NULL){
+    //     cout <<"Letra: "<<pesquisa <<" encontrada na posicao: "<<txt1 - txt2+1<<endl;
+    // }else{
+    //     cout << "Letra: "<<pesquisa<< " nao encontrada";
+    // }
     return 0;
 }
