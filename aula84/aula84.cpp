b3399cc7844a5ffc3fe8dc39b4f988005b41ad13
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-05 08:15:45 +0200

    aula 84 feita com sucesso

--- aula84/aula84.cpp
+++ aula84/aula84.cpp
@@ -2,10 +2,14 @@
 #include <string>
 using namespace std;
 
-int main(){
-string txt("Curso de c++ no canal cfbcursos!");
-
-cout << txt<<endl;
+int main()
+{
+    string txt("Curso de c++ no canal cfbcursos!");
+    txt.capacity();
+    txt.resize(10);
+    txt.shrink_to_fit();
+    cout<< txt<<endl;
+    cout << txt.capacity();
 
     return 0;
 }
