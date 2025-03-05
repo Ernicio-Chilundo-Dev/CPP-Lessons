96d480979a458f9a46b6e3597f9e1949ce677e84
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-05 11:31:51 +0200

    aula 87 feita com sucesso aprendi a trabalhar com a biblioteca array e as suas funcoes

--- aula87/aula87.cpp
+++ aula87/aula87.cpp
@@ -4,8 +4,32 @@ using namespace std;
 
 int main(){
     array<int,10>vt={2,3,4,5,1,6,8,7,0,9};
+    // for(auto it =vt.rbegin();it!=vt.rend(); it++){
+    //     cout << *it<<endl;
+    // }
+
+    // cout << vt.size()<<endl;
+    // cout << vt.max_size()<<endl;
+
+    // if(vt.empty()){
+    //     cout <<"Array Vazia!";
+    // }else{
+    //     cout << "Array nao esta vazia";
+    // }
+
+    array<int,10>cfb;
+    cfb.fill(2);
+
+    vt.swap(cfb);
+
+    for(auto x:cfb){
+        cout << x<< " - ";
+    }
+
+    cout <<endl;
+
     for(auto x:vt){
-        cout << x<<endl;
+        cout<<x<<" - ";
     }
     return 0;
-}
+} 
