88f5743e225f515fa5d341435ad86d9f6d56e90e
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-05 13:31:32 +0200

    aula 90 feita com sucesso

--- aula90/aula90.cpp
+++ aula90/aula90.cpp
@@ -2,13 +2,27 @@
 #include <set>
 using namespace std;
 
-int main(){
-set<int>st1={4,5,6,7,8,2,3,9,0};
+int main()
+{
+    set<int> st1 = {4, 5, 6, 7, 8, 2, 3, 9, 0};
 
-for(auto x:st1){
-    cout <<x<< " -> ";
-}
+    for (auto x : st1)
+    {
+        cout << x << " -> ";
+    }
 
+    cout << endl;
+    set<int> set2 = {3, 2, 1, 0, 5, 6, 7, 8, 9};
+
+    if (set2.count(22) != 0)
+    {
+        cout << "Esta no container!";
+    }
+    else
+    {
+        cout << "Nao esta no container!";
+    }
+
+    //LOWER_BOUND|UPPER_BOUND
     return 0;
 }
-
