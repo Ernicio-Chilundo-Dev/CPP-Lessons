62b1741c6f7422cc4b2542d79c28071f8a5b9b94
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-05 12:35:53 +0200

    aula 88 feita com sucesso

--- aula88/aula88.cpp
+++ aula88/aula88.cpp
@@ -2,14 +2,22 @@
 #include <vector>
 using namespace std;
 
-int main(){
-    vector<int>vct={2,4,5,6,7,8,9,0,1,3};
-    
-    for (auto it = vct.begin(); it!=vct.end(); it++)
+int main()
+{
+    vector<int> vct = {2, 4, 5, 6, 7, 8, 9, 0, 1, 3};
+
+    for (auto it = vct.begin(); it != vct.end(); it++)
     {
-        cout << *it<< " - ";
+        cout << *it << " - ";
     }
-    
+
+    cout << endl;
+
+    vct.resize(5);
+    for (auto x : vct)
+    {
+        cout << x << " - ";
+    }
 
     return 0;
 }
