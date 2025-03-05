87c234b6d65b7138a13844e19f8aef0e4f5a6216
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-05 13:00:27 +0200

    aula 89 feita com sucesso aprendi a trabalhar com funcao merge e reverse

--- aula89/aula89.cpp
+++ aula89/aula89.cpp
@@ -2,18 +2,29 @@
 #include <list>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    list<int>list1={3,5,7,8,9,0,4,3,5,6,0};
+    // list<int>list1={3,5,7,8,9,0,4,3,5,6,0};
 
-    list1.sort();
-    list1.unique();
+    // list1.sort();
+    // list1.unique();
 
-    for (auto y:list1)
+    // for (auto y:list1)
+    // {
+    //     cout << y<<endl;
+    // }
+
+    list<int> list1 = {0, 1, 2, 3, 4};
+    list<int> list2 = {5, 6, 7, 8, 9};
+
+    list1.merge(list2);
+    list1.reverse();
+
+    for (auto i : list1)
     {
-        cout << y<<endl;
+        cout << i << endl;
     }
-    
 
     return 0;
 }
