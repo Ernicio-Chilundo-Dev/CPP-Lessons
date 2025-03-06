50858e279d2c55839e0539de55ad7db564f1862e
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-06 13:41:52 +0200

    A biblioteca stack em C++ faz parte da STL (Standard Template Library) e fornece a estrutura de dados pilha (stack). Ela segue o princípio LIFO (Last In, First Out), ou seja, o último elemento inserido é o primeiro a ser removido.

--- aula92/aula92.cpp
+++ aula92/aula92.cpp
@@ -6,21 +6,26 @@ int main(){
 
     stack<int>pilha1,pilha2;
 
-    pilha1.push(10);
-    pilha1.push(20);
-    pilha1.push(30);
-    pilha1.push(40);
-    pilha1.push(50);
-
+    
+    pilha2.push(10);
+    pilha2.push(20);
+    pilha2.push(30);
+    pilha2.push(40);
+    pilha2.push(50);
+    
+    pilha2.swap(pilha1);
+    cout << "Elementos da pilha: "<< pilha1.size()<<endl;
     while(!pilha1.empty()){
         cout << pilha1.top()<<endl;
         pilha1.pop();
     }
 
     if(pilha1.empty()){
-        cout << "Pilha vazia!";
+        cout << "Pilha vazia!"<<endl;
     }else{
-        cout << "Pilha contem valores";
+        cout << "Pilha contem valores"<<endl;
     }
+
+    cout <<"Elementos da pilha: "<<pilha1.size()<<endl;
     return 0;
 }
