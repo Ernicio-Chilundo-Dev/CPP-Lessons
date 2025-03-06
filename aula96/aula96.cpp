3aadbddc59260ea708579063caf62a79ee22392a
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-06 23:44:03 +0200

    iniciado a aula 96 que trata a biblioteca iterador

---
+++ aula96/aula96.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+int main(){
+    vector<int>vt={1,2,3,4,10,5,6,7,8,9};
+    vector<int>vt2={11,12,14};
+    vector<int>it,it1,it2;
+
+    return 0;
+}
