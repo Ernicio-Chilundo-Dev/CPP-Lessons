2c7f8886d3e491a67c34bd7e8151f2cf39106198
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-07 02:30:54 +0200

    aula feita com sucesso

--- aula98/aula98.cpp
+++ aula98/aula98.cpp
@@ -24,6 +24,34 @@ int main(){
     }
 
     cout << "\n\n";
+
+    //remove = Remove um valor indicado da colecao
+    cout << "REMOVE"<<endl;
+    vector<int>vt3={2,1,2,3,2,4,2,5,2,6,2,7,2,8,2,9,2};
+    int qtde = count(vt3.begin(),vt3.end(),2);
+    remove(vt3.begin(),vt3.end(),2);
+    vt3.resize(vt3.size()-qtde);
+    vt3.shrink_to_fit();
+    for(auto x: vt3){
+        cout << x<<" ";
+    }
+
+    cout <<"\n\n";
+
+    //unique = remove elementos duplicados consecutivos na colecao
+    cout << "Unique"<<endl;
+    vector<int>vt4={0,1,2,3,3,3,4,5,2,6,7,8,9};
+    vector<int>::iterator it;
+    it = unique(vt4.begin(),vt4.end());
+    vt4.resize(distance(vt4.begin(),it));
+    for(auto x:vt4){
+        cout<<x<<" ";
+    }
+
+    cout << "\n\n";
+
+    
+
     
 
     return 0;
