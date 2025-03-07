0f4d4386b9787e06f78ceca174c906e45f5a2343
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-07 09:35:20 +0200

    aula feita com sucesso

--- aula102/aula102.cpp
+++ aula102/aula102.cpp
@@ -6,5 +6,16 @@ int main(){
     pair<int,string>par;
     par= make_pair(24,"Ernicio");
     cout << par.first << " - "<<par.second<<endl;
+
+    vector<int>vt={1,2,4,0,6,9};
+    for(auto x:vt){
+        cout << x<<"  ";
+    }
+    cout <<endl;
+
+    double x = 20;
+    cout << pow(x,2);
+
+    cout <<endl;
     return 0;
 }
