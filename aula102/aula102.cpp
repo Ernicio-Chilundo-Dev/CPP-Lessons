3ab3cadf88373fc3bca5b364073b93d65980545a
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-07 09:25:57 +0200

    iniciado a aula 102 que fala da biblioteca bits/stdc++.h, que permiti o uso de todas as biblioteca nele mesmo sem fazer o include da das outras bibliotecas

---
+++ aula102/aula102.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int,string>par;
+    par= make_pair(24,"Ernicio");
+    cout << par.first << " - "<<par.second<<endl;
+    return 0;
+}
